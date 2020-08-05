07d2d16a99c97f09598f1a40877aebaca010e0fe
Eric Peterson <ericjamesp.2009@gmail.com>
2020-08-05 10:36:57 -0500

    single responsibiliy...

---
+++ DatabaseConnectionManager.java
@@ -0,0 +1,24 @@
+// import java.sql.SQLException;
+
+public class DatabaseConnectionManager {
+    // private Connection conn;
+    // private static DatabaseConnectionManager connectionInstance = new
+    // DatabaseConnectionManager();
+    // private DatabaseConnectionManager() {
+
+    // }
+    // public static DatabaseConnectionManager getManagerInstance() {
+    // return connectionInstance;
+    // }
+
+    // // contains connection details
+
+    // // @throws SQLException
+    // public void connect() throws SQLException {
+    // conn = DriverManager.getConnection("Some/Database/URL");
+    // System.out.println("Established Database Connection...");
+    // }
+    // public Connection getConnectionObject() {
+    // return conn;
+    // }
+}
